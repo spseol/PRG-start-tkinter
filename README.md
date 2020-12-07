21fee365a75bcfb2c90743830f3429723aabdb0f
Marrek Nožka <marek@tlapicka.net>
2020-12-07 08:05:38 +0100

    Init

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# PRG -- 
+# PRG -- TkInter start
 
 [![Join the chat at gitter.im/spseol/Python](https://badges.gitter.im/spseol/PRG-No.svg)](https://gitter.im/spseol/Python?utm_source=share-link&utm_medium=link&utm_campaign=share-link)
 
-* abc
-# start-tkinter
+
+*  [main.py](main.py)
---
+++ main.py
@@ -0,0 +1,25 @@
+from os.path import basename, splitext
+import tkinter as tk
+
+# from tkinter import ttk
+
+
+class Application(tk.Tk):
+    name = basename(splitext(basename(__file__.capitalize()))[0])
+    name = "Foo"
+
+    def __init__(self):
+        super().__init__(className=self.name)
+        self.title(self.name)
+        self.bind("<Escape>", self.quit)
+        self.lbl = tk.Label(self, text="Hello World")
+        self.lbl.pack()
+        self.btn = tk.Button(self, text="Quit", command=self.quit)
+        self.btn.pack()
+
+    def quit(self, event=None):
+        super().quit()
+
+
+app = Application()
+app.mainloop()
